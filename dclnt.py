3c3e1a2f1db7165bb9b5d845406c760f2ed01b25
Adwiz <adwiz@suroviy.ru>
2022-01-13 07:06:54 +0300

    successfully refactored

--- dclnt.py
+++ dclnt.py
@@ -4,12 +4,12 @@ import collections
 
 from nltk import pos_tag
 
-
+@profile
 def flat(_list):
     """ [(1,2), (3,4)] -> [1, 2, 3, 4]"""
     return sum([list(item) for item in _list], [])
 
-
+@profile
 def is_verb(word):
     if not word:
         return False
@@ -19,7 +19,7 @@ def is_verb(word):
 
 Path = ''
 
-
+@profile
 def get_trees(_path, with_filenames=False, with_file_content=False):
     filenames = []
     trees = []
@@ -49,15 +49,15 @@ def get_trees(_path, with_filenames=False, with_file_content=False):
     print('trees generated')
     return trees
 
-
+@profile
 def get_all_names(tree):
     return [node.id for node in ast.walk(tree) if isinstance(node, ast.Name)]
 
-
+@profile
 def get_verbs_from_function_name(function_name):
     return [word for word in function_name.split('_') if is_verb(word)]
 
-
+@profile
 def get_all_words_in_path(path):
     trees = [t for t in get_trees(path) if t]
     function_names = [f for f in flat([get_all_names(t) for t in trees]) if
@@ -69,7 +69,7 @@ def get_all_words_in_path(path):
     return flat([split_snake_case_name_to_words(function_name)
                  for function_name in function_names])
 
-
+@profile
 def get_top_verbs_in_path(path, top_size=10):
     global Path
     Path = path
@@ -83,7 +83,7 @@ def get_top_verbs_in_path(path, top_size=10):
                   for function_name in fncs])
     return collections.Counter(verbs).most_common(top_size)
 
-
+@profile
 def get_top_functions_names_in_path(path, top_size=10):
     t = get_trees(path)
     nms = [f for f in
