5d85d7e0d2d4c343e114efecee2d035a56a1565b
Aleksei Elokhin <aleksei.e@clips4sale.com>
2023-02-16 09:01:15 +0200

    35 lesson in progress 56:43

--- mp.py
+++ mp.py
@@ -1,10 +1,16 @@
-import objgraph
-# from guppy import hpy
+# import objgraph
+# # from guppy import hpy
+# #
+# # h = hpy()
+# #
+# # print(h.heap())
 #
-# h = hpy()
-#
-# print(h.heap())
+# x = []
+# y = [x, [x], dict(x=x)]
+# objgraph.show_refs([y], filename='sample-graph.png')
 
-x = []
-y = [x, [x], dict(x=x)]
-objgraph.show_refs([y], filename='sample-graph.png')
+srt = "Once upon a time, there was a magical pig who lived in a small town. He was known for his magical " \
+      "abilities and used them to help the townsfolk with all of their problems. \
+      One day, the pig figured out a way to turn pumpkins into sports cars! Everyone was so amazed by this feat, " \
+      "they all decided to buy pumpkins from the pig and turn them"
+print(len(srt))
---
+++ threading_example.py
@@ -0,0 +1,92 @@
+# import logging
+import threading
+from multiprocessing.pool import ThreadPool
+from time import sleep, time
+from loguru import logger
+import requests
+
+
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+
+def process_google_results(query: str) -> int:
+    url = 'https://www.google.com/search'
+    # logger.info(f'Starting searching for {query}')
+    logger.info('Starting searching for {!r}', query)
+    # logger.info('Starting searching for %s', query)
+    response = requests.get(
+        url=url,
+        params={'q': f'{query}'},
+        # timeout=(5, 10),
+    )
+    logger.info('Got answer for {}', query)
+    text = response.text
+    # result = parse_search_results_for_query(query, text)
+    # save_result_to_db(result)
+    sleep(1)
+    response_len = len(text)
+    logger.info('Finished processing query {!r} with result len {}', query, response_len)
+    if response_len > 10_000:
+        try:
+            1/0
+        except ZeroDivisionError:
+            logger.exception('Error')
+            return None
+    return response_len
+
+
+if __name__ == "__main__":
+    logger.info('Starting main')
+    # # query = 'python'
+    # # process_google_results(query)
+    # queries = (
+    #     'iphone',
+    #     'galaxy',
+    #     'huawei',
+    # )
+    # threads = []
+    # for query in queries:
+    #     thread = threading.Thread(
+    #         target=process_google_results,
+    #         args=(query, ),
+    #     )
+    #     logger.info('query {!r} start', query)
+    #     thread.start()
+    #     logger.info('query {!r} finish', query)
+    #     threads.append((thread, query))
+    # #
+    # for thread, query in threads:
+    #     logger.info('Joining thread {}', query)
+    #     thread.join()
+    #     logger.info('Finished thread {}', query)
+    # thread = threading.Thread(
+    #     target=process_google_results,
+    #     args=('python',),
+    #     daemon=True,
+    #     # daemon=False,
+    # )
+    # thread.start()
+    # thread.join()
+    queries = (
+        'iphone',
+        'galaxy',
+        'xiaomi',
+        'huawei',
+        'pycharm',
+        'jetbrains',
+        'vscode',
+        'yandex',
+        'google',
+    )
+    queries *= 2
+    start = time()
+    pool = ThreadPool(9)
+    results = pool.map(process_google_results, queries)
+
+    end = time()
+
+    logger.info('pool took {:.3f} seconds', end - start)
+    logger.info('results: {}', results)
+    logger.info('Finished main')
+
